0d26f117a6256593adb5e52303e2dc4620a1025b
arun2254 <arun.yellapu7@gmail.com>
2017-07-03 09:33:29 -0700

    Initial Commit of the project - RESTFull Webservice

---
+++ src/com/youtube/rest/status/V1_status.java
@@ -0,0 +1,24 @@
+package com.youtube.rest.status;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/v1/status")
+public class V1_status {
+	
+	private static final String version="10.00.01";
+	
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnTitle(){
+		return "<p>Java Web Service-Arun</p>";
+	}
+	
+	@Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVersion(){
+		return "<p>API Version is " + version +"</p>";
+	}
+
+}
